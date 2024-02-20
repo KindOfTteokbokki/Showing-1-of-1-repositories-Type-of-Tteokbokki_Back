d6ce7d8bdfe551cd24c9d45cfff0a0f0cc0661df
BigHwang <gxd0214@gmail.com>
2024-02-21 00:52:25 +0900

    오류 수정

--- src/main/java/halfandhalf/domain/ST0010/dao/ST0010Dao.java
+++ src/main/java/halfandhalf/domain/ST0010/dao/ST0010Dao.java
@@ -1,7 +1,5 @@
 package halfandhalf.domain.ST0010.dao;
 
-import halfandhalf.domain.QA0010.dto.QA0011Dto;
-import halfandhalf.domain.QA0010.dto.QA0012Dto;
 import halfandhalf.domain.ST0010.dto.ST0010Dto;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
